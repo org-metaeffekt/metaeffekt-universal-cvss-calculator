999fc351658aa1fcac902850712de6ea1464c96d
YanWittmann <mail@yanwittmann.de>
2026-02-27 09:31:08 +0100

    #14 adjust publishing script

--- publish-package.sh
+++ publish-package.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
+PUBLISH_ARG="$1"
 PUBLISH_MODE="--dry-run"
-if [ "$1" == "publish" ]; then
+
+if [ "${PUBLISH_ARG}" == "publish" ]; then
     PUBLISH_MODE=""
     echo "!!! REAL PUBLISH MODE ENABLED !!!"
 else
@@ -15,19 +17,15 @@ npm ci
 npm run build
 
 TARBALL_NAME=$(npm pack)
-TARBALL_PATH="$(pwd)/$TARBALL_NAME"
-
-echo "Generated tarball: $TARBALL_NAME"
+TARBALL_PATH="$(pwd)/${TARBALL_NAME}"
 
 cd ../calculator-package-test
 rm -rf node_modules package-lock.json
-npm install "$TARBALL_PATH" --no-save
+npm install "${TARBALL_PATH}" --no-save
 npm test
 
-cd ../ae-cvss-calculator
-npm publish "$TARBALL_PATH" $PUBLISH_MODE
-
-cd ../
-rm "ae-cvss-calculator/$TARBALL_NAME"
+cd ..
+npm publish "${TARBALL_PATH}" ${PUBLISH_MODE}
+rm "${TARBALL_PATH}"
 
 echo "Publish workflow complete."
